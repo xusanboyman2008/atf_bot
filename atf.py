9438e4832e24fb4f0719d168a1102bf02fd31f96
Xusanboyman <abdulkhaevtolibjon55@gmail.com>
2026-03-01 07:27:46 +0500

    Modify payload and adjust worker settings
    Updated payload data and increased worker count and request frequency.

--- atf.py
+++ atf.py
@@ -12,16 +12,16 @@ HEADERS = {
 }
 
 payload = {
-    "initData": "query_id=AAHgh7YIAwAAAOCHtghwLjBR&user=%7B%22id%22%3A6588631008%2C%22first_name%22%3A%22%28%E2%96%BA__%E2%97%84%29%20T_T%20X_X%20xusanboyman%22%2C%22last_name%22%3A%22%F0%9F%87%BA%F0%9F%87%BF%22%2C%22username%22%3A%22xusanboyman200%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FABKucBBOPE9qSGZbrWEF4xW6wrAlil-YqDxjQfABvEOlAI0lJIBU15Q2npDYdUbN.svg%22%7D&auth_date=1772258279&signature=EMzd8yqNq_Q9ThK1r3WEW0eu139vRh-F1w1SjDbxYmHxlSeKaNSmVoDxyxsn1_hlkogqAIGjOh9egJlzk_YjDQ&hash=75569e955b2592f3860d50cf9dd47b3206246057f724fc9ddae48aa55fe644fb",
-    "tg_id": "6588631008",
-    "taps": 20
+    "initData": "query_id=AAHgh7YIAwAAAOCHtgjXhctz&user=%7B%22id%22%3A6588631008%2C%22first_name%22%3A%22%28%E2%96%BA__%E2%97%84%29%20T_T%20X_X%20xusanboyman%22%2C%22last_name%22%3A%22%F0%9F%87%BA%F0%9F%87%BF%22%2C%22username%22%3A%22xusanboyman200%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FABKucBBOPE9qSGZbrWEF4xW6wrAlil-YqDxjQfABvEOlAI0lJIBU15Q2npDYdUbN.svg%22%7D&auth_date=1772331253&signature=35KZujKAwHvpgMxPmqq0pGjPvolGcDOviw3vM7Q7iIL87T-kqt1yLHIAy01g3ll4jy0y0FEM-OimD2-uyGtFBg&hash=8dcfca1dc3a14af71328e84c2db18ff58564fe33f7b06d94c3f37d7f89d44e48",
+    "tg_id": "6859957139",
+    "taps": 22
 }
 
 DATA = json.dumps(payload)
 
 # ✅ SAFE VALUES FOR SHARED HOSTING
-WORKERS = 8  # do NOT increase much
-BASE_DELAY = 0.15  # request spacing
+WORKERS = 800  # do NOT increase much
+BASE_DELAY = 0.001  # request spacing
 
 
 async def worker(name, session):
@@ -31,24 +31,7 @@ async def worker(name, session):
 
                 # read response so connection is reused
                 await resp.read()
-
-                if resp.status == 200:
-                    print(f"[{name}] OK")
-                    await asyncio.sleep(
-                        BASE_DELAY + random.uniform(0.05, 0.15)
-                    )
-
-                elif resp.status == 429:
-                    print(f"[{name}] RATE LIMITED")
-                    await asyncio.sleep(3)
-
-                else:
-                    print(f"[{name}] STATUS:", resp.status)
-                    await asyncio.sleep(1)
-
-        except asyncio.TimeoutError:
-            print(f"[{name}] timeout")
-            await asyncio.sleep(2)
+                print(resp.status)
 
         except aiohttp.ClientError as e:
             print(f"[{name}] network error:", e)
@@ -84,5 +67,4 @@ async def main():
         await asyncio.gather(*tasks)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
