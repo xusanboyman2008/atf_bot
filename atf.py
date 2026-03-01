415a7ee7b75e8ffe0f40a618091fd5d4e5aa749f
Xusanboyman <abdulkhaevtolibjon55@gmail.com>
2026-03-01 08:12:20 +0500

    Add files via upload

--- atf.py
+++ atf.py
@@ -4,7 +4,7 @@ import random
 
 import aiohttp
 
-URL = "https://atfminers.asloni.online/miner/index.php?action=sync_taps&t=1772280042728"
+URL = "https://atfminers.asloni.online/miner/index.php?action=sync_taps&t=1772333419892"
 
 HEADERS = {
     "accept": "application/json, text/plain, */*",
@@ -12,16 +12,15 @@ HEADERS = {
 }
 
 payload = {
-    "initData": "query_id=AAHgh7YIAwAAAOCHtgjXhctz&user=%7B%22id%22%3A6588631008%2C%22first_name%22%3A%22%28%E2%96%BA__%E2%97%84%29%20T_T%20X_X%20xusanboyman%22%2C%22last_name%22%3A%22%F0%9F%87%BA%F0%9F%87%BF%22%2C%22username%22%3A%22xusanboyman200%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FABKucBBOPE9qSGZbrWEF4xW6wrAlil-YqDxjQfABvEOlAI0lJIBU15Q2npDYdUbN.svg%22%7D&auth_date=1772331253&signature=35KZujKAwHvpgMxPmqq0pGjPvolGcDOviw3vM7Q7iIL87T-kqt1yLHIAy01g3ll4jy0y0FEM-OimD2-uyGtFBg&hash=8dcfca1dc3a14af71328e84c2db18ff58564fe33f7b06d94c3f37d7f89d44e48",
-    "tg_id": "6859957139",
-    "taps": 22
+    "initData": "query_id=AAHgh7YIAwAAAOCHtggUCi16&user=%7B%22id%22%3A6588631008%2C%22first_name%22%3A%22%28%E2%96%BA__%E2%97%84%29%20T_T%20X_X%20xusanboyman%22%2C%22last_name%22%3A%22%F0%9F%87%BA%F0%9F%87%BF%22%2C%22username%22%3A%22xusanboyman200%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FABKucBBOPE9qSGZbrWEF4xW6wrAlil-YqDxjQfABvEOlAI0lJIBU15Q2npDYdUbN.svg%22%7D&auth_date=1772333403&signature=Az6iIK-AmexRFpYTMAa5z6CmWlPQt0Oi80nzFzCTttzgYxBl6PqEJRqFzkaStpL5o8EzzeAF9KvjzQ6HM0Q4BQ&hash=d0b4d725d7e2b2c0fe70e56b481aa4f4ad53e8c4cb6077f974715a9fb75c8624",
+    "tg_id": "6588631008",
+    "taps": 20
 }
-
 DATA = json.dumps(payload)
 
 # ✅ SAFE VALUES FOR SHARED HOSTING
-WORKERS = 800  # do NOT increase much
-BASE_DELAY = 0.001  # request spacing
+WORKERS = 500  # do NOT increase much
+BASE_DELAY = 0  # request spacing
 
 
 async def worker(name, session):
@@ -32,23 +31,16 @@ async def worker(name, session):
                 # read response so connection is reused
                 await resp.read()
                 print(resp.status)
-
+                if resp.status == 429:
+                    await asyncio.sleep(3)
         except aiohttp.ClientError as e:
             print(f"[{name}] network error:", e)
             await asyncio.sleep(3)
 
 
 async def main():
-    timeout = aiohttp.ClientTimeout(
-        total=15,
-        connect=5,
-        sock_read=10
-    )
-
     # ✅ LIMITED connector (critical for Alwaysdata)
     connector = aiohttp.TCPConnector(
-        limit=20,
-        limit_per_host=10,
         ssl=False,
         ttl_dns_cache=300,
         enable_cleanup_closed=True,
@@ -56,7 +48,6 @@ async def main():
 
     async with aiohttp.ClientSession(
             headers=HEADERS,
-            timeout=timeout,
             connector=connector
     ) as session:
         tasks = [
